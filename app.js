c7560f34144db604353dedd912923196d0ac1938
Antriksh rawat <156641834+unihack2@users.noreply.github.com>
2024-04-21 03:22:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,218 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+const baseurl = "https://v6.exchangerate-api.com/v6/ee75d0d114b1a272b2a950fc/latest/";
+let select = document.querySelectorAll(".main select");
+const from = document.querySelector(".from select");
+const to = document.querySelector(".to select");
+let msg = document.querySelector(".exrate p");
+for (let sel of select) {
+  for (code in countryList) {
+    let newop = document.createElement("option");
+    newop.innerText = code;
+    newop.value = code;
+    if (sel.name === "from" && code === "USD") {
+      newop.selected = "selected";
+    } else if (sel.name === "to" && code === "INR") {
+      newop.selected = "selected";
+    }
+    sel.append(newop);
+  }
+  sel.addEventListener("change", (evt) => {
+    changeflag(evt.target);
+  })
+}
+const changeflag = (element) => {
+  let cntrycode = countryList[element.value];
+  let newsrc = 'https://flagsapi.com/' + cntrycode + '/flat/64.png';
+  let img = element.parentElement.querySelector("img");
+  img.src = newsrc;
+};
+let btn = document.getElementById("exbt");
+btn.addEventListener("click", async () => {
+  msg.addEventListener("change",()=>{
+    alert("exchange rate not available");
+  })
+  let amt = document.querySelector("#ip");
+  let amtval = amt.value;
+  if (amtval === "" || amtval < 1) {
+    amtval = 1;
+    amt.value = "1";
+  }
+  let url = baseurl + from.value;
+  let response = await fetch(url);
+  let data = await response.json();
+  for (cn in countryList) {
+    if (to.value === cn) {
+      let rate = data.conversion_rates[cn];
+      let finalamt = amtval * rate;
+      msg.innerText = amtval + ' ' + from.value + '  =  ' + finalamt + ' ' + to.value;
+      if (finalamt !== NaN) {
+        console.log();
+      }else {
+        alert("exchange rate unavalable");
+      }
+    }
+  }
+
+  amtval = 1;
+  amt.value = "1";
+})
